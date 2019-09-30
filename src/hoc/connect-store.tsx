cdcc04cb3c5d21f18283e0b9b597abe933b1046c
Wouter van Heeswijk <wouter.vanheeswijk@tomtom.com>
2019-09-30 14:28:43 +0200

    Added another HOC

---
+++ src/hoc/connect-store.tsx
@@ -0,0 +1,30 @@
+import * as React from 'react';
+import { Store, Subscription } from 'stuffit';
+
+export const connectToProps = <T extends {}>(store: Store<T>) => <P extends T>(Component: React.ComponentType<P>) =>
+    class extends React.PureComponent<Omit<P, keyof T>, T> {
+        public state = store.state;
+
+        private subscription: Subscription | undefined = undefined;
+
+        public componentDidMount() {
+            if (!this.subscription) {
+                this.subscription = store.subscribe(this.handleChange);
+            }
+        }
+
+        public componentWillUnmount() {
+            if (this.subscription) {
+                this.subscription.unsubscribe();
+                this.subscription = undefined;
+            }
+        }
+
+        public render() {
+            return <Component {...this.props as P} {...this.state} />;
+        }
+
+        private handleChange = (value: T) => {
+            this.setState(value);
+        }
+    } as React.ComponentClass<Omit<P, keyof T>, T>;
