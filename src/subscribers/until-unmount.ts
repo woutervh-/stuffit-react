de6024943213d4b729890dedf4fd693275666cd6
Wouter van Heeswijk <wouter.vanheeswijk@tomtom.com>
2019-11-04 16:20:35 +0100

    Subscribe until unmount with options

--- src/subscribers/until-unmount.ts
+++ src/subscribers/until-unmount.ts
@@ -1,9 +1,10 @@
 import { Store } from 'stuffit';
 
-export const subscribeUntilUnmount = <T>(component: React.Component, store: Store<T>, callback: (value: T) => void, immediate?: boolean) => {
-    const subscription = store.subscribe(callback, immediate);
-    const originalComponentWillUnmount = component.componentWillUnmount;
-    component.componentWillUnmount = function () {
+export const subscribeUntilUnmount = <T>(options: { component: React.Component, store: Store<T>, callback?: (value: T) => void, immediate?: boolean }) => {
+    const callback = options.callback || (() => options.component.forceUpdate());
+    const subscription = options.store.subscribe(callback, options.immediate);
+    const originalComponentWillUnmount = options.component.componentWillUnmount;
+    options.component.componentWillUnmount = function () {
         subscription.unsubscribe();
         if (originalComponentWillUnmount) {
             originalComponentWillUnmount.apply(this);
