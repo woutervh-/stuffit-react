5f01a4922f7dfefd767a9c85d6a5fddc4a0c341c
Wouter van Heeswijk <wouter.vanheeswijk@tomtom.com>
2020-02-28 15:00:05 +0100

    Added documentation to subscribeUntilUnmount

--- src/subscribers/until-unmount.ts
+++ src/subscribers/until-unmount.ts
@@ -1,5 +1,17 @@
 import { Store } from 'stuffit';
 
+/**
+ * Subscribes to the given store.
+ * State changes will be dispatched to the given callback.
+ * The subscription is unsubscribed from when the given component will unmount.
+ * 
+ * If callback is undefined, it will simply trigger a `forceUpdate` call on the given component.
+ * 
+ * @param options.component Component which will be checked for unmounting to cancel the subscription.
+ * @param options.store Store to listen for state changes on.
+ * @param options.callback Callback which receives state changes. If undefined, the state changes will trigger `forceUpdate` on the component.
+ * @param options.immediate Whether or not to trigger the callback immediately after subscribing.
+ */
 export const subscribeUntilUnmount = <T>(options: { component: React.Component, store: Store<T>, callback?: (value: T) => void, immediate?: boolean }) => {
     const callback = options.callback || (() => options.component.forceUpdate());
     const subscription = options.store.subscribe(callback, options.immediate);
